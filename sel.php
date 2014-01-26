c2e95c02efe6cd7bd278948d4b1d87c60bcf87f0
Lucas Teske <lucas@teske.com.br>
2014-01-26 11:13:43 -0200

    Added github flag to selector

--- sel.php
+++ sel.php
@@ -105,6 +105,7 @@
 	</script>
 </head>
 <body onLoad="Init();">
+<a href="https://github.com/racerxdl/piuvisual"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png" alt="Fork me on GitHub"></a>
 <center>
 <div id="videoHolder"></div>
 <BR><BR>
