433213e8a16e3cdccf742ccb0e6bf04c457e1c47
Lucas Teske <lucas@teske.com.br>
2014-06-11 20:04:49 -0300

    Added comments to pump.js

--- jspump/pump.js
+++ jspump/pump.js
@@ -29,8 +29,16 @@
     may have different license including but not limited to JPAK, jQuery and others.
 
 */
+/*
+ * This is the main file of Piuvisual
+ */
+
 var PUMPER = function() {};
-PUMPER.Version = 1.1;
+PUMPER.Version = 1.5;
+
+/**
+ * 	Adds a trim prototype if there is no trim on Strings
+ */
 if (!String.prototype.trim) {
     String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
 
@@ -41,31 +49,37 @@ if (!String.prototype.trim) {
     String.prototype.fulltrim=function(){return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');};
 };
 
+/*
+ * 	Global variables
+ */
 PUMPER.Globals = {
-    DrawAnchors     :   false,
-    Drawer          :   undefined,
-    Looper          :   undefined,
-    Skin            :   undefined,
-    CurrentBPM      :   0,
-    CurrentCombo    :   0,
-    EnableSound     :   true,
-    EnableVideo     :   false,
-    Double          :   false,
-    EffectBlock     :   [],
-    DisableLog      :   false,
-    EnableDebug     :   true,
-    ObjectsLoaded   :   0,
-    ObjectsToLoad   :   0,
-    AllLoaded       :   false,
-    CheckLoaded     :   function()  {
+    DrawAnchors     :   false,			//	Draw Object Anchors (Only for Non-WebGL)
+    Drawer          :   undefined,		//	Global Drawer Holder
+    Looper          :   undefined,		//	Global Looper Holder
+    Skin            :   undefined,		//	Global Skin Holder
+    CurrentBPM      :   0,				//	Current Music BPM
+    CurrentCombo    :   0,				//	Current Combo
+    EnableSound     :   true,			//	Enable Sound
+    EnableVideo     :   false,			//	Enable Video
+    Double          :   false,			//	Song is Double
+    EffectBlock     :   [],				//	Effect Block Holder
+    DisableLog      :   false,			//	Disable Logging
+    EnableDebug     :   true,			//	Enable Debug
+    ObjectsLoaded   :   0,				//	Objects Loaded
+    ObjectsToLoad   :   0,				//	Objects to Load
+    AllLoaded       :   false,			//	If All is Loaded
+    CheckLoaded     :   function()  {	//	Global Function to check if all objects is loaded
         if(PUMPER.Globals.ObjectsLoaded == PUMPER.Globals.ObjectsToLoad)    
             PUMPER.Globals.AllLoaded = true;
         return PUMPER.Globals.AllLoaded;
     },
-    WebGL           :   false,
-    glExperimental  :   false
+    WebGL           :   false,			//	Running on WebGL Context
+    glExperimental  :   false			//	WebGL is on Experimental Version
 };
 
+/*
+ * 	Returns true if the browser supports WebGL
+ */
 function hasWebGL() {
     var canvas = document.createElement('canvas');
     try { gl = canvas.getContext("webgl", { failIfMajorPerformanceCaveat : true }); }catch (x) { gl = null; }
@@ -78,36 +92,53 @@ function hasWebGL() {
 
 //PUMPER.Globals.WebGL = hasWebGL();
 PUMPER.Globals.WebGL = false; // Disabled for now
+
+/*
+ * Logging function
+ */
 PUMPER.log    =   function(msg)   {
-    //  TODO
     if(!PUMPER.Globals.DisableLog)
         console.log("PUMPER(log):> "+msg);
 };
+
+/*
+ * 	Debug Logging Function
+ */
 PUMPER.debug  =   function(msg)   {
-    //  TODO
     if(PUMPER.Globals.EnableDebug)
         console.log("PUMPER(debug):> "+msg);
 };
 
+/*
+ * 	Fixes an int to 3 decimal cases left padding by 7
+ */
 PUMPER.FixTo3	=	function(val)	{
 	return PUMPER.Pad(val.toFixed(3),7);
 };
+
+/*
+ * 	Left pad the number by size
+ */
 PUMPER.Pad		=	function(num, size) {
-    var s = "000000000" + num;
+    var s = "000000000000" + num;
     return s.substr(s.length-size);
 };
+
+/*
+ * 	Updates the infohead if exists
+ */
 PUMPER.UpdateInfoHead	=	function()	{
     if($("#infohead") !== undefined)
     	$("#infohead").html("BPM: "+PUMPER.Globals.CurrentBPM+" BEAT "+PUMPER.FixTo3(PUMPER.Globals.Game.notedata.CurrentBeat)+" TIME: "+PUMPER.FixTo3(PUMPER.Globals.Music.GetTime())+" BLOCK: "+PUMPER.Pad(PUMPER.Globals.Game.notedata.CurrentBPMChange,3)+" SPEED: "+PUMPER.ScrollSpeed+"x");
 };
 
 //  Skin Properties
-PUMPER.ArrowSize                = 64;
-PUMPER.ShowWidth                = 50;
-PUMPER.OffsetY                  = 32;
+PUMPER.ArrowSize                = 64;		//	The arrow size. Fixed at 64 by the noteskin
+PUMPER.ShowWidth                = 50;		//	The width to draw. This affects the spacing between notes
+PUMPER.OffsetY                  = 32;		//	The Offset of the receiver.
 
 //  Game Properties
-PUMPER.ScrollSpeed              = 3;
+PUMPER.ScrollSpeed              = 3;		//	The Scroll Speed
 
 //  Note Types
 PUMPER.NoteNull                 = 0;
@@ -157,21 +188,30 @@ PUMPER.ModAppear          =   3;
 PUMPER.ModHighJudge       =   4;
 PUMPER.ModStandBreak      =   5;
 
-
+/*
+ * 	Clones a canvas context
+ */
 PUMPER.cloneCanvas = function ( canvas ) {
     var newCanvas = document.createElement('canvas');
     newCanvas.width = canvas.width;
     newCanvas.height = canvas.height;
     context.drawImage(canvas, 0, 0);
-    var context = newCanvas.getContext('2d');
     return newCanvas;
-}
+};
+
+/*
+ *	Creates a canvas buffer
+ */
 PUMPER.createBuffCanvas = function ( width, height)   {
     var canvas = document.createElement('canvas');
     canvas.width = width;
     canvas.height = height;
     return canvas;   
-}
+};
+
+/*
+ * 	Crops a image
+ */
 PUMPER.CropImage = function(image,x,y,width,height)   {
       var canvas = document.createElement('canvas');
       canvas.width = width;
@@ -179,7 +219,12 @@ PUMPER.CropImage = function(image,x,y,width,height)   {
       var context = canvas.getContext('2d');
       context.drawImage(image,x,y,width,height,0,0,width,height);
       return canvas;
-}
+};
+
+/*
+ * 	Cut and crop an image
+ */
+
 PUMPER.CropImageTarget = function(image,x,y,width,height,targetx,targety,targetw,targeth)   {
       var canvas = document.createElement('canvas');
       canvas.width = targetw;
@@ -187,7 +232,11 @@ PUMPER.CropImageTarget = function(image,x,y,width,height,targetx,targety,targetw
       var context = canvas.getContext('2d');
       context.drawImage(image,x,y,width,height,targetx,targety,width,height);
       return canvas;
-}
+};
+
+/*
+ * 	Resizes an Image
+ */
 PUMPER.ResizeImage = function(image,width,height)   {
       var canvas = document.createElement('canvas');
       canvas.width = width;
@@ -195,11 +244,13 @@ PUMPER.ResizeImage = function(image,width,height)   {
       var context = canvas.getContext('2d');
       context.drawImage(image,0,0,image.width,image.height,0,0,width,height);
       return canvas;
-}
-PUMPER.PadInt = function (num, size) {
-    var s = "000000000000" + num;
-    return s.substr(s.length-size);
-}
+};
+
+PUMPER.PadInt = PUMPER.Pad;	//	Alias
+
+/*
+ * 	Gets the compatible codecs for video and audio elements
+ */
 PUMPER.GetCompatibleCodecs = function()  {
     var testEl = document.createElement( "video" ), testEl2 = document.createElement( "audio" ),
     video=[],audio=[];
@@ -228,8 +279,11 @@ PUMPER.GetCompatibleCodecs = function()  {
             audio.push("ogg");
     }
     return { "video": video, "audio": audio };
-}
+};
 
+/*
+ * 	Parses the URL Hashes 
+ */
 PUMPER.parseHashes = function () {
 	HashConfig = { "UCS" : undefined, "Speed" : 2};
 	hashes = window.location.hash.split("#");
@@ -246,6 +300,9 @@ PUMPER.parseHashes = function () {
 	return HashConfig;
 };
 
+/*
+ * 	Does a 240FPS Refresh rate. Alternative to requestAnimFrame
+ */
 PUMPER.HighSpeedRequest = (function(){
 	  return  function( callback ){
 			    window.setTimeout(callback, 1000 / 240);
